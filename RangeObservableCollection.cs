6d740dcf669a8fefdddd0bb13277de52bd63c327
Evgeniy Plyusnin <eninbox@gmail.com>
2013-09-12 16:02:53 +0600

    Добавлена поддержка группового изменения. Однако, как оказалось, базовая ObservableCollection один хрен этого не поддерживает :(

--- RangeObservableCollection.cs
+++ RangeObservableCollection.cs
@@ -25,7 +25,17 @@ namespace ObjectiveCommons.Collections
             if (NotificationsSupressCounter <= 0)
                 base.OnCollectionChanged(e);
             else
+            {
                 NotificationsAwaiting = true;
+#if ACTIONS_WITH_RANGE
+                if (e.Action == NotifyCollectionChangedAction.Add)
+                    for (int i = 0; i < e.NewItems.Count; i++)
+                        RegisterNewItem(e.NewStartingIndex + i, e.NewItems[i]);
+                if (e.Action == NotifyCollectionChangedAction.Remove)
+                    for (int i = 0; i < e.OldItems.Count; i++)
+                        RegisterOldItem(e.OldStartingIndex + i, e.OldItems[i]);
+#endif
+            }
         }
 
         /// <summary>
@@ -37,7 +47,7 @@ namespace ObjectiveCommons.Collections
             if (elements == null)
                 throw new ArgumentNullException("elements");
 
-            using (Locker())
+            using (var l = Locker())
             {
                 foreach (T item in elements)
                 {
@@ -46,11 +56,22 @@ namespace ObjectiveCommons.Collections
             }
         }
 
+        private System.Collections.ArrayList NewItems { get; set; }
+        private int? NewItemsIndex { get; set; }
+        private System.Collections.ArrayList OldItems { get; set; }
+        private int? OldItemsIndex { get; set; }
+        private NotifyCollectionChangedAction? lockedaction { get; set; }
+
         /// <summary>
         /// Заставляет коллекцию подавить нотификации
         /// </summary>
         protected void SupressNotifications()
         {
+            if (NotificationsSupressCounter == 0)
+            {
+                NewItems = new System.Collections.ArrayList(); NewItemsIndex = null;
+                OldItems = new System.Collections.ArrayList(); OldItemsIndex = null;
+            }
             NotificationsSupressCounter++;
         }
         /// <summary>
@@ -63,10 +84,77 @@ namespace ObjectiveCommons.Collections
             if (NotificationsSupressCounter <= 0)
             {
                 if (NotificationsAwaiting)
+                {
+#if ACTIONS_WITH_RANGE
+                    NotifyCollectionChangedEventArgs e;
+                    switch (lockedaction ?? NotifyCollectionChangedAction.Reset)
+                    {
+                        case NotifyCollectionChangedAction.Add:
+                            e = new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, NewItems, NewItemsIndex.Value);
+                            break;
+                        case NotifyCollectionChangedAction.Remove:
+                            e = new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, OldItems, OldItemsIndex.Value);
+                            break;
+                        default:
+                            e = new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset);
+                            break;
+                    }
+                    OnCollectionChanged(e);
+#else
                     OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+#endif
+                }
                 NotificationsAwaiting = false;
             }
         }
+
+        public bool RegisterNewItem(int index, object item)
+        {
+            bool isSequent = false;
+            if (NewItemsIndex == null)
+            {
+                NewItemsIndex = index;
+                isSequent = true;
+            }
+            else if (index >= NewItemsIndex && index <= NewItemsIndex + NewItems.Count + 1)
+            {
+                isSequent = true;
+            }
+            NewItems.Add(item);
+            if (lockedaction == null) lockedaction = NotifyCollectionChangedAction.Add;
+            bool allGood = isSequent && lockedaction == NotifyCollectionChangedAction.Add;
+            if (!allGood) lockedaction = NotifyCollectionChangedAction.Reset;
+            return allGood;
+        }
+        public bool RegisterOldItem(int index, object item)
+        {
+            bool isSequent = false;
+            if (OldItemsIndex == null || index == OldItemsIndex - 1)
+            {
+                OldItemsIndex = index;
+                isSequent = true;
+            }
+            else if (index == OldItemsIndex)
+            {
+                isSequent = true;
+            }
+            OldItems.Add(item);
+            if (lockedaction == null) lockedaction = NotifyCollectionChangedAction.Remove;
+            bool allGood = isSequent && lockedaction == NotifyCollectionChangedAction.Remove;
+            if (!allGood) lockedaction = NotifyCollectionChangedAction.Reset;
+            return allGood;
+        }
+
+        //protected override void InsertItem(int index, T item)
+        //{
+        //    base.InsertItem(index, item);
+        //    RegisterNewItem(index, item);
+        //}
+        //protected override void RemoveItem(int index)
+        //{
+        //    RegisterOldItem(index, Items[index]);
+        //    base.RemoveItem(index);
+        //}
 
         /// <summary>
         /// Объект подавления нотификаций в коллекции.
