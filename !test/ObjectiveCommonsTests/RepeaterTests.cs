a8f3eecf785fdc3d213e4c3571e7714f8006943e
Evgeniy Plyusnin <eninbox@gmail.com>
2013-11-21 16:32:17 +0600

    Добавлен класс Repeater и модульные тесты к нему Добавлен проект с модульными тестами

---
+++ !test/ObjectiveCommonsTests/RepeaterTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObjectiveCommons;
+
+namespace ObjectiveCommonsTests
+{
+    [TestClass]
+    public class RepeaterTests
+    {
+        private class CustomException : Exception { }
+        private class CustomExceptionChild : CustomException { }
+        private class CustomException2 : Exception { }
+
+        [TestMethod]
+        public void WithoutExceptions()
+        {
+            Repeater
+                .Repeat(() => Thread.Sleep(10))
+                .Catch<CustomException>()
+                .Build().Run();
+        }
+
+        private class ThrowAndCatchResult
+        {
+            public int RunCounter = 0;
+            public int CatchCounter = 0;
+// ReSharper disable once ConvertToConstant.Local
+            public int ThrowCount = 10;
+        }
+        private ThrowAndCatchResult ThrowAndCatch<TThrow, TCatch>(int throwCount = 10)
+            where TThrow : Exception, new()
+            where TCatch : Exception, new()
+        {
+            var res = new ThrowAndCatchResult() {ThrowCount = throwCount};
+            Repeater
+                .Repeat(() =>
+                {
+                    Thread.Sleep(3);
+                    res.RunCounter++;
+                    if (res.RunCounter <= res.ThrowCount) throw new TThrow();
+                })
+                .Catch<TCatch>(c => res.CatchCounter++)
+                .Build().Run();
+            return res;
+        }
+
+        [TestMethod]
+        public void CatchingCustomException()
+        {
+            var res = ThrowAndCatch<CustomException, CustomException>();
+            Assert.AreEqual(res.ThrowCount, res.CatchCounter, "Отловили сообщений меньше, чем ожидали");
+            Assert.AreEqual(res.ThrowCount + 1, res.RunCounter, "Должны были сработать без ошибки на {0} раз, а сработали на {1}", res.ThrowCount + 1, res.RunCounter);
+        }
+        [TestMethod]
+        public void CatchingCustomExceptionChild()
+        {
+            var res = ThrowAndCatch<CustomExceptionChild, CustomException>();
+            Assert.AreEqual(res.ThrowCount, res.CatchCounter, "Отловили сообщений меньше, чем ожидали");
+            Assert.AreEqual(res.ThrowCount + 1, res.RunCounter, "Должны были сработать без ошибки на {0} раз, а сработали на {1}", res.ThrowCount + 1, res.RunCounter);
+        }
+        [TestMethod]
+        public void ThrowForeignException()
+        {
+            try
+            {
+                ThrowAndCatch<CustomException2, CustomException>();
+                Assert.Fail("Должны были вылететь по исключению");
+            }
+            catch (CustomException2)
+            {
+                // Тест проиден
+                return;
+            }
+            catch (Exception)
+            {
+                Assert.Fail("Пробросилось какое-то не такое исключение");
+            }
+        }
+    }
+}
---
+++ Repeater.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ObjectiveCommons
+{
+    /// <summary>
+    /// Повторяет указанное действие при возниконвении указанных исключений
+    /// </summary>
+    public class Repeater
+    {
+        private abstract class ExceptionCapture
+        {
+            public abstract Type ExceptionType { get; }
+            public abstract void OnCaptured(Exception exc);
+        }
+
+        private class ExceptionCapture<TException> : ExceptionCapture
+            where TException : Exception
+        {
+            public override Type ExceptionType { get { return typeof (TException); } }
+            public Action<TException> CatchAction { get; private set; }
+
+            public override void OnCaptured(Exception exc) { if (CatchAction != null) CatchAction((TException) exc); }
+
+            public ExceptionCapture(Action<TException> CatchAction)
+            {
+                this.CatchAction = CatchAction;
+            }
+        }
+
+        public TimeSpan Timeout { get; private set; }
+        public int MaximumAttempts { get; set; }
+        private IList<ExceptionCapture> CatchingExceptions { get; set; }
+
+        public Action Job { get; private set; }
+
+        /// <summary>
+        /// Возвращает строителя нового повторителя
+        /// </summary>
+        /// <param name="Job">Повторяемое действие</param>
+        /// <returns>Возвращает строителя для создания повторителя</returns>
+        public static RepeaterBuilder Repeat(Action Job)
+        {
+            return new RepeaterBuilder(Job);
+        }
+
+        private Repeater(Action Job) { this.Job = Job; }
+
+        /// <summary>
+        /// Запускает процесс
+        /// </summary>
+        public void Run()
+        {
+            int counter = 0;
+            var sw = new Stopwatch();
+            sw.Start();
+
+            while (sw.Elapsed < Timeout || counter < MaximumAttempts)
+            {
+                try
+                {
+                    Job();
+                    break;
+                }
+                catch (Exception e)
+                {
+                    var t = e.GetType();
+                    var capture = CatchingExceptions.FirstOrDefault(c => t == c.ExceptionType || t.IsSubclassOf(c.ExceptionType));
+                    if (capture == null) throw;
+                    else capture.OnCaptured(e);
+                }
+                counter++;
+            }
+        }
+
+        public class RepeaterBuilder
+        {
+            public Action Job { get; private set; }
+            public TimeSpan Timeout { get; private set; }
+            public int MaximumAttempts { get; set; }
+            private IList<ExceptionCapture> CatchingExceptions { get; set; }
+
+            internal RepeaterBuilder(Action Job)
+            {
+                this.Job = Job;
+                Timeout = TimeSpan.MaxValue;
+                MaximumAttempts = int.MaxValue;
+                CatchingExceptions = new List<ExceptionCapture>();
+            }
+
+            /// <summary>
+            /// Создаёт повторителя с заданными параметрами
+            /// </summary>
+            public Repeater Build()
+            {
+                if (Job == null) throw new ArgumentException("Не выбрана работа");
+                return new Repeater(Job)
+                       {
+                           Timeout = Timeout,
+                           MaximumAttempts = MaximumAttempts,
+                           CatchingExceptions = CatchingExceptions
+                       };
+            }
+            /// <summary>
+            /// Указывает на необходимость прервать повторения по таймауту
+            /// </summary>
+            /// <param name="Timeout">Таймаут операции</param>
+            public RepeaterBuilder WithTimeout(TimeSpan Timeout)
+            {
+                this.Timeout = Timeout;
+                return this;
+            }
+            /// <summary>
+            /// Указывает на необходимость прервать повторения при достижении заданного количества неудачных попыток
+            /// </summary>
+            /// <param name="Count">Максимальное количество неудачных повторений</param>
+            public RepeaterBuilder WithMaximumAttempts(int Count)
+            {
+                this.MaximumAttempts = Count;
+                return this;
+            }
+            /// <summary>
+            /// Добавляет отлавливаемое исключение
+            /// </summary>
+            /// <typeparam name="TException">Тип отлавливаемого исключения</typeparam>
+            /// <param name="CatchAction">Действие, которое необходимо выполнить при возникновении этого исключения</param>
+            public RepeaterBuilder Catch<TException>(Action<TException> CatchAction = null) where TException : Exception
+            {
+                CatchingExceptions.Add(new ExceptionCapture<TException>(CatchAction));
+                return this;
+            }
+        }
+    }
+}
